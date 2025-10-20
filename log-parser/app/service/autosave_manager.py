ea3a65e82ea493f56597d9be93d5dcbb5a37885c
njm2360 <me@njm2360.com>
2025-10-20 13:40:11 +0900

    Fix duplicate save skip

--- log-parser/app/service/autosave_manager.py
+++ log-parser/app/service/autosave_manager.py
@@ -1,8 +1,10 @@
 import asyncio
 import aiohttp
 import logging
+from hashlib import sha256
 from datetime import datetime, timedelta
 
+from app.model.mmp_savedata import MmpSaveRecord, MmpSaveData
 from app.utils.cloudsave_state_store import CloudSaveState, CloudSaveStateStore
 
 
@@ -27,29 +29,45 @@ class AutoSaveManager:
             return
 
         now = datetime.now()
+        user_id = record.user_id
+        url = record.raw_url
 
-        record = await self._cloud_state_store.get(user_id)
+        state = await self._cloud_state_store.get(user_id)
 
-        if record:
-            last_credit_all = record.credit_all
-            last_attempt_at = record.last_attempt_at or datetime.min
+        if state:
+            last_credit_all = state.credit_all
+            last_attempt_at = state.last_attempt_at
+            last_data_hash = state.data_hash
 
             # 成功・失敗にかかわらず、前回の試行時刻を基準にレート制御
-            if now - last_attempt_at < timedelta(seconds=self._save_interval):
+            if not ignore_rate_limit and now - last_attempt_at < timedelta(
+                seconds=self._save_interval
+            ):
                 return
 
             # 総獲得が巻き戻っている場合はセーブしない
             if credit_all < last_credit_all:
                 logging.warning(
-                    f"[CloudSave] Data rollback detected - Skiping (user={user_id})"
+                    f"[CloudSave] Data rollback detected - Skipping (user={user_id})"
                 )
                 return
+
+            # データが同一の場合セーブしない
+            data_hash = self._calc_data_hash(record.data)
+
+            if data_hash == last_data_hash:
+                logging.warning(
+                    f"[CloudSave] Save skipped (no data change) (user={user_id})"
+                )
+                return
+
+        else:
+            data_hash = self._calc_data_hash(record.data)
 
         await self._cloud_state_store.update(
             user_id,
             CloudSaveState(
-                credit_all=credit_all,
-                last_attempt_at=now,
+                credit_all=credit_all, last_attempt_at=now, data_hash=data_hash
             ),
         )
 
@@ -80,3 +98,7 @@ class AutoSaveManager:
             logging.error(f"[CloudSave] Request timed out")
         except Exception as e:
             logging.exception(f"[CloudSave] Unexpected error occured: {e}")
+
+    def _calc_data_hash(self, data: MmpSaveData) -> str:
+        data_json = data.model_dump_json()
+        return sha256(data_json.encode("utf-8")).hexdigest()
